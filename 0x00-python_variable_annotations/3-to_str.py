254acdbed373c0d3a62a12821d8c9fe351ab0903
beloveyeboah <beloveyeboah@gmail.com>
2024-10-10 14:29:25 -0700

    five

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -4,6 +4,7 @@
 function to_str that takes a float n as argument
 """
 
+
 def to_str(n: float) -> str:
     """
     eturns the string representation of the float.
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+function sum_list which takes a list input_list
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    returns their sum as a float.
+    """
+
+    total = sum(input_list)
+    return (total)

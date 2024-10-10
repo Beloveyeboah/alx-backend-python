7c85ed178fbfb9b7233de7440892af665c09ee88
beloveyeboah <beloveyeboah@gmail.com>
2024-10-10 11:51:34 -0700

    one

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" concat function
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """return string concatenation of
+    two strings"""
+
+    new = str1 + str2
+    return "{}".format(new)

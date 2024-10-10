1ea1ad8e05323855edc3057f8a90dcb4f94037fa
beloveyeboah <beloveyeboah@gmail.com>
2024-10-10 11:44:20 -0700

    one

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
+
+"""add function
 """
-add function to return sum
-in a float """
 
 
 def add(a: float, b: float) -> float:
-    """sum of a and b in flaot"""
+    """return sum in float
+    """
 
-    total = a + b
-    return (total)
+    return a + b

c02dbeb176335e31ccee0377a2044bf3c6ea35ff
beloveyeboah <beloveyeboah@gmail.com>
2024-10-10 14:01:46 -0700

    floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -2,10 +2,14 @@
 
 import math
 
-floor = __import__('2-floor').floor
+"""
+ype-annotated function floor which takes a float n
+"""
 
-ans = floor(3.14)
 
-print(ans == math.floor(3.14))
-print(floor.__annotations__)
-print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
+def floor(n: float) -> float:
+    """
+    returns the floor of the float.
+    """
+
+    return math.floor(n)
